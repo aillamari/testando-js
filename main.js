85dd60d51624bda737cf4597131e67bee73b5847
aillamari <127351793+aillamari@users.noreply.github.com>
2024-02-27 10:19:41 -0300

    ooi

--- main.js
+++ main.js
@@ -5,4 +5,11 @@ document.querySelector("#p1").style.color="red"
 function mudarCorFundo(){
     document.querySelector("#p1").style.background="red"
 }
+function alternarTexto(){ 
+    texoForm= 
+    document.querySelector("textimput").value
+    document.querySelector("texto2").innerHTML.HTML
+    =textoForm
+}
+
  
